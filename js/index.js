8bcbbc68fe17f49f1cec637ce2519ad4e4419ca4
Fire株式会社 <83343186+anto565@users.noreply.github.com>
2021-08-16 19:27:18 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        message: "ロード中..",
+        seen: false
+    },
+    methods: {
+        move() {
+            //音楽//
+            const se = new Audio("sound/enter.mp3")
+            se.play()
+            this.seen = true
+                //アニメーション//
+            this.seen = true
+            setTimeout(() => {
+                this.message = "ロード中..."
+            }, 800)
+
+            setTimeout(() => {
+                this.message = "ロード中.."
+            }, 1600)
+
+            setTimeout(() => {
+                this.message = "ロード中..."
+            }, 2400)
+
+            setTimeout(() => {
+                this.message = "ロード中.."
+            }, 3200)
+
+            setTimeout(() => {
+                location.href = "bit-error"
+            }, 4000)
+        }
+    }
+})
